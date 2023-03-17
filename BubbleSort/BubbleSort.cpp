7ec837f70b3dd8df7c916bc8339fa6ea366f26fe
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-03-17 16:31:43 +0700

    Memperbaiki procedure menampilkan hasil

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -28,13 +28,13 @@ void input()        //Procedure untuk input
     }
 }
 
-void display() {                                    //Procedure untuk menampilkan hasil
-    cout << endl;                                              //Output baris kosong
-    cout << "====================" << endl;                    //Output ke layar
-    cout << "Element array yang telah tersusun " << endl;      //Output ke layar
-    cout << "====================" << endl;                    //Output ke layar
-    for (int j = 0; j < n; j++) {                   //Looping dengan i dimulai dari 0 hingga n-1
-        cout << a[j] << endl;                       //Output ke layar 
+void display() {                            //Procedure untuk menampilkan hasil
+    cout << endl;                                           //Output baris kosong
+    cout << "====================" << endl;                 //Output ke layar
+    cout << "Element Array yang telah tersusun" << endl;    //Output ke layar
+    cout << "====================" << endl;                 //Output ke layar
+    for (int j = 0; j < n; j++) {           //Looping dengan i dimulai dari 0 hingga n-1
+        cout << a[j] << endl;               //Output ke layar
     }
-    cout << endl;                                   //Output baris kosong
+    cout << endl;                           //Output baris kosong
 }
